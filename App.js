37c64aafd520b73db928ec3ef64b81ea2c6a44db
IvanDF <delfatti.ivan@gmail.com>
2022-04-08 23:57:59 +0200

    feat: add react React Navigation library and init navigation stack

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
 import CategoriesScreen from "./screens/CategoriesScreen";
 
+const Stack = createStackNavigator();
+
 export default function App() {
   return (
     <>
-      <StatusBar style="inverted" />
-      <CategoriesScreen />
+      <StatusBar style="auto" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
